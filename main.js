9658b42f391be1ab1a03f4951b7306a877c6999b
Sinan Sulaiman <143943603+sinan-sulaiman-33@users.noreply.github.com>
2024-06-09 15:32:48 +0530

    Update and rename main.js to script.js

--- main.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector("button");
-button.addEventListener("click", updateName);
-function updateName(){
-  const name = prompt("Enter the new name:");
-  button.textContent = `Student-1: ${name}`;
-}
